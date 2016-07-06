cc11b8c129d5012c3f98302094e94a9cdaa6eef2
zhanghanlin <andyzhanghanlin@163.com>
2016-07-06 17:31:23 +0800

    add mybatis

---
+++ src/main/java/com/demo/boot/conf/StaticResourceConfig.java
@@ -0,0 +1,14 @@
+package com.demo.boot.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class StaticResourceConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+    }
+}
