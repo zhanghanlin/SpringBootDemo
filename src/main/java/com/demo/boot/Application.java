43c704e26db3bd4e717bf75f360afa1881fac692
zhanghanlin <andyzhanghanlin@163.com>
2016-09-19 20:22:07 +0800

    Spring boot 集成shiro

--- src/main/java/com/demo/boot/Application.java
+++ src/main/java/com/demo/boot/Application.java
@@ -2,10 +2,10 @@ package com.demo.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Controller
+@RestController
 @EnableWebMvc
 @SpringBootApplication
 public class Application {
--- src/main/java/com/demo/boot/controller/ShiroController.java
+++ src/main/java/com/demo/boot/controller/ShiroController.java
@@ -6,6 +6,7 @@ import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@EnableAutoConfiguration
 @RestController
 public class ShiroController {
 
