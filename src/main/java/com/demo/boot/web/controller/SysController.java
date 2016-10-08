887c0577238bd8395696b1221b4eca1f293212ae
zhanghanlin <andyzhanghanlin@163.com>
2016-10-08 14:35:14 +0800

    权限修改

--- src/main/java/com/demo/boot/web/controller/SysController.java
+++ src/main/java/com/demo/boot/web/controller/SysController.java
@@ -62,11 +62,11 @@ public class SysController {
             //在调用了login方法后,SecurityManager会收到AuthenticationToken,并将其发送给已配置的Realm执行必须的认证检查
             //每个Realm都能在必要时对提交的AuthenticationTokens作出反应
             //所以这一步在调用login(token)方法时,它会走到MyRealm.doGetAuthenticationInfo()方法中,具体验证方式详见此方法
-            LOG.info("对用户[" + username + "]进行登录验证..验证开始");
             if (StringUtils.isBlank(username) || StringUtils.isBlank(login.getPassword())) {
                 LOG.info("请输入用户名或密码");
                 redirectAttributes.addFlashAttribute("message", "请输入用户名或密码");
             } else {
+                LOG.info("对用户[" + username + "]进行登录验证..验证开始");
                 currentUser.login(token);
                 LOG.info("对用户[" + username + "]进行登录验证..验证通过");
             }
